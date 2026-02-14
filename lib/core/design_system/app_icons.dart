96d8b67150334a866f21af2a49b9e7b3a6878b8a
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-02-14 15:38:38 +0900

    chore: #1 아이콘 저장

---
+++ lib/core/design_system/app_icons.dart
@@ -0,0 +1,26 @@
+class AppIcons {
+  AppIcons._();
+
+  static const String _basePath = 'assets/icons';
+
+  static const String add = '$_basePath/ic_add.svg';
+  static const String bell = '$_basePath/ic_bell.svg';
+  static const String chatDots = '$_basePath/ic_chat-dots.svg';
+  static const String creditCard = '$_basePath/ic_credit-card.svg';
+  static const String eye = '$_basePath/ic_eye.svg';
+  static const String heart = '$_basePath/ic_heart.svg';
+  static const String home = '$_basePath/ic_home.svg';
+  static const String mail = '$_basePath/ic_mail.svg';
+  static const String map = '$_basePath/ic_map.svg';
+  static const String menu = '$_basePath/ic_menu.svg';
+  static const String mobile = '$_basePath/ic_mobile.svg';
+  static const String more = '$_basePath/ic_more.svg';
+  static const String protect = '$_basePath/ic_protect.svg';
+  static const String questionCircle = '$_basePath/ic_question-circle.svg';
+  static const String search = '$_basePath/ic_search.svg';
+  static const String settings = '$_basePath/ic_settings.svg';
+  static const String settingsLine = '$_basePath/ic_settings-line.svg';
+  static const String twoLine = '$_basePath/ic_two-line-horizontal.svg';
+  static const String user = '$_basePath/ic_user.svg';
+  static const String wallet = '$_basePath/ic_wallet.svg';
+}
